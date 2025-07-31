b61e8776eb9b99345a3c01a023f15979e4439a95
sir-w7 <vohwinston@gmail.com>
2025-07-31 16:56:50 -0700

    Added more debug options, unity build, and made build script more generic

--- build.bat
+++ build.bat
@@ -1,17 +1,23 @@
 @echo off
 
+if "%VSCMD_VER%"=="" (
+	call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+
 if not exist build mkdir build
 
 pushd build
 
-set INCLUDE_DIR=/I..\include /I..\include\imgui /I..\include\imgui\backends
-set IMGUI_DIR=..\include\imgui
+set INCLUDE_DIR=/I..\src\ext /I..\src\ext\imgui /I..\src\ext\imgui\backends
+set IMGUI_DIR=..\src\ext\imgui
 set IMGUI_SRC=%IMGUI_DIR%\imgui.cpp %IMGUI_DIR%/imgui_demo.cpp %IMGUI_DIR%/imgui_draw.cpp %IMGUI_DIR%/imgui_tables.cpp %IMGUI_DIR%/imgui_widgets.cpp %IMGUI_DIR%/backends/imgui_impl_sdlrenderer3.cpp %IMGUI_DIR%/backends/imgui_impl_sdl3.cpp
 
-set COMPILE_FLAGS=%INCLUDE_DIR% /EHsc /DPLATFORM_WIN32 /MT
+set COMPILE_FLAGS=/Z7 /EHsc /DPLATFORM_WIN32
 set LINK_FLAGS=/LIBPATH:..\lib\win32\SDL3 /LIBPATH:..\lib\win32\opencv SDL3.lib opencv_world4120.lib
 
-cl %COMPILE_FLAGS% ..\src\narcissus.cpp %IMGUI_SRC% /link %LINK_FLAGS%
+REM cl %INCLUDE_DIR% %COMPILE_FLAGS% ..\src\narcissus.cpp %IMGUI_SRC% /link %LINK_FLAGS%
+
+cl %INCLUDE_DIR% %COMPILE_FLAGS% ..\src\narcissus.cpp /link %LINK_FLAGS%
 
 copy ..\lib\win32\SDL3\SDL3.dll .
 copy ..\lib\win32\opencv\opencv_world4120.dll .
--- src/narcissus.cpp
+++ src/narcissus.cpp
@@ -20,11 +20,15 @@
 
 #include "opencv2/opencv.hpp"
 
-// Unity building this because the Makefile doesn't accommodate the module
-// compilation of a single file too well (only the entire directory).
-#include "imgui.h"
-#include "imgui_impl_sdl3.h"
-#include "imgui_impl_sdlrenderer3.h"
+// Bruh let's just unity build bc why not?
+#include "imgui.cpp"
+#include "imgui_tables.cpp"
+#include "imgui_widgets.cpp"
+#include "imgui_draw.cpp"
+//#include "imgui_demo.cpp"
+
+#include "imgui_impl_sdl3.cpp"
+#include "imgui_impl_sdlrenderer3.cpp"
 
 #include "layer.h"
 
@@ -45,68 +49,81 @@ int main()
 {
     setvbuf(stdout, 0, _IONBF, 0);
     setvbuf(stderr, 0, _IONBF, 0);
-
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        fprintln(stderr, "Failed to initialize video.");
-        return -1;
-    }
-
-    SDL_Window *window = 0;
-    SDL_Renderer *renderer = 0;
-
-    f32 main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
-
-    if (!SDL_CreateWindowAndRenderer("Narcissus",
-                                     (int)(1280*main_scale), (int)(720*main_scale),
-                                     SDL_WINDOW_RESIZABLE|SDL_WINDOW_HIGH_PIXEL_DENSITY,
-                                     &window, &renderer)) {
-        fprintln(stderr, "Failed to create window."); //
-        return -1;
-    }
-    SDL_RaiseWindow(window);
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-
-    ImGui::StyleColorsDark();
-    ImGuiStyle& style = ImGui::GetStyle();
-    style.ScaleAllSizes(main_scale);
-    style.FontScaleDpi = main_scale;
-
-    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
-    ImGui_ImplSDLRenderer3_Init(renderer);
-
+    
 #ifdef WEBCAM
     cv::VideoCapture cap(WEBCAM);
-
+    
     if (cap.isOpened() == false) {
         fprintln(stderr, "Cannot open webcam.");
         return -1;
     }
-
+    
     f64 w = cap.get(cv::CAP_PROP_FRAME_WIDTH);
     f64 h = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-
+    
     println("Webcam dimensions: %f x %f", w, h);
 #else
     cv::Mat image;
     cv::imread("../res/sfgate.jpg", image, cv::IMREAD_GRAYSCALE);
     assert(!image.empty());
 #endif
-
+    
     cap.set(cv::CAP_PROP_FPS, 60);
+	
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        fprintln(stderr, "Failed to initialize video.");
+        return -1;
+    }
+    
+    SDL_Window *window = 0;
+    SDL_Renderer *renderer = 0;
+    
+    f32 main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    
+    if (!SDL_CreateWindowAndRenderer("Narcissus",
+                                     (int)(2*w*main_scale), (int)(2*h*main_scale),
+                                     SDL_WINDOW_RESIZABLE|SDL_WINDOW_HIGH_PIXEL_DENSITY,
+                                     &window, &renderer)) {
+        fprintln(stderr, "Failed to create window."); //
+        return -1;
+    }
+
+    SDL_RaiseWindow(window);
+    
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    
+    ImGui::StyleColorsDark();
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);
+    style.FontScaleDpi = main_scale;
+    
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+    
+    
     bool running = true;
-   
+    
+    i32 scale_interpolation = 0;
+
     cv::Mat tmp; 
     cap.read(tmp);
-    SDL_Texture* tex = SDL_CreateTexture(renderer,
+    SDL_Texture *tex_resized = SDL_CreateTexture(renderer,
+                                         SDL_PIXELFORMAT_BGR24,
+                                         SDL_TEXTUREACCESS_STREAMING,
+                                         64, 64);
+    SDL_Texture *tex_full = SDL_CreateTexture(renderer,
                                          SDL_PIXELFORMAT_BGR24,
                                          SDL_TEXTUREACCESS_STREAMING,
                                          tmp.cols, tmp.rows);
-    defer { SDL_DestroyTexture(tex); };
 
+    SDL_SetTextureScaleMode(tex_resized, SDL_SCALEMODE_NEAREST);
+    defer { SDL_DestroyTexture(tex_resized); };
+    defer { SDL_DestroyTexture(tex_full); };
+    
+    bool greyscale = true, blur = true, canny = true, resize = true;
     u64 time_elapsed = 0; 
     auto prev = SDL_GetTicks(); 
     while (running) {
@@ -114,13 +131,13 @@ int main()
         while (SDL_PollEvent(&event)) {
             ImGui_ImplSDL3_ProcessEvent(&event);
             switch (event.type) {
-            case SDL_EVENT_QUIT:
-                running = false;
-                break;
-            default: break;
+                case SDL_EVENT_QUIT:
+                    running = false;
+                    break;
+                default: break;
             }
         }
-
+        
 #ifdef WEBCAM
         cv::Mat frame;
         if(!cap.read(frame)) {
@@ -128,55 +145,104 @@ int main()
             break;
         }
 #endif
-
-        cv::Mat greyscale, blurred, edge, resized;
-
+        
+        cv::Mat working_mat = frame.clone(), tmp_mat = frame;
+        
 #ifdef WEBCAM
-        cv::cvtColor(frame, greyscale, cv::COLOR_BGR2GRAY);
+        if (greyscale) {
+            cv::cvtColor(tmp_mat, working_mat, cv::COLOR_BGR2GRAY);
+            tmp_mat = working_mat.clone();
+        }
 #else
         //cv::cvtColor(image, greyscale, cv::COLOR_BGR2GRAY);
-        greyscale = image;
+        working_mat = image;
 #endif
-        cv::GaussianBlur(greyscale, blurred, cv::Size(5, 5), 0);
-        cv::Canny(blurred, edge, lower_threshold, upper_threshold);
+        if (blur) {
+            cv::GaussianBlur(tmp_mat, working_mat, cv::Size(5, 5), 0);
+            tmp_mat = working_mat.clone();
+        }
+
+        if (resize) {
+            cv::resize(tmp_mat, working_mat, cv::Size(64, 64), 0, 0, scale_interpolation);
+            tmp_mat = working_mat.clone(); 
+        }
+        
+        if (canny){
+            cv::Canny(tmp_mat, working_mat, lower_threshold, upper_threshold);
+            tmp_mat = working_mat.clone(); 
+        }
 
         cv::Mat final;
-        cv::cvtColor(edge, final, cv::COLOR_GRAY2BGR);
 
-        // cv::resize(edge, resized, cv::Size(64, 64), cv::INTER_AREA);
-        // cv::imshow(window, resized);
-        // cv::imshow(window, edge);
-        //cv::imshow(window, edge);
-        //
-        SDL_UpdateTexture(tex, 0, (void *)final.data, final.step1());
+        if (greyscale) {
+            cv::cvtColor(tmp_mat, final, cv::COLOR_GRAY2BGR);
+        } else {
+            final = working_mat;
+        }
 
+        if (resize) {
+            SDL_UpdateTexture(tex_resized, 0, (void *)final.data, final.step1());
+        } else {
+            SDL_UpdateTexture(tex_full, 0, (void *)final.data, final.step1());
+        }
+        
         ImGui_ImplSDLRenderer3_NewFrame();
         ImGui_ImplSDL3_NewFrame();
         ImGui::NewFrame();
-
+        
         {
             ImGui::Begin("Debug Window");
             defer { ImGui::End(); };
-
+            
             ImGui::Text("%d FPS", (int)(1.0f/((f32)time_elapsed/1000.f)));
             ImGui::Text("Time since last frame: %lld ms", time_elapsed);
             ImGui::SliderInt("Lower threshold", &lower_threshold, 0, 255);
             ImGui::SliderInt("Upper threshold", &upper_threshold, 0, 255);
-        }
 
+            ImGui::Spacing();
+
+            if (ImGui::CollapsingHeader("Scaling interpolation options")) {
+                ImGui::RadioButton("INTER_NEAREST", &scale_interpolation, 0);
+                ImGui::RadioButton("INTER_LINEAR", &scale_interpolation, 1);
+                ImGui::RadioButton("INTER_CUBIC", &scale_interpolation, 2);
+                ImGui::RadioButton("INTER_AREA", &scale_interpolation, 3);
+                ImGui::RadioButton("INTER_LANCZOS4", &scale_interpolation, 4);
+                ImGui::RadioButton("INTER_LINEAR_EXACT", &scale_interpolation, 5);
+                ImGui::RadioButton("INTER_NEAREST_EXACT", &scale_interpolation, 6);
+                // ImGui::RadioButton("INTER_MAX", &scale_interpolation, 7);
+                // ImGui::RadioButton("INTER_FILL_OUTLIERS", &scale_interpolation, 8);
+                // ImGui::RadioButton("INTER_INVERSE_MAP", &scale_interpolation, 16);
+            }
+            
+            ImGui::Spacing();
+
+            if (ImGui::CollapsingHeader("Processing options")) {
+                ImGui::Checkbox("Greyscale", &greyscale);
+                ImGui::Checkbox("Blur", &blur);
+                ImGui::Checkbox("Resize", &resize);
+                ImGui::Checkbox("Canny", &canny);
+            }
+
+        }
+        
         ImGui::Render();
         SDL_SetRenderScale(renderer,
                            io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
         SDL_SetRenderDrawColorFloat(renderer, 0.0f, 0.0f, 0.0f, 0.0f);
         SDL_RenderClear(renderer);
+        
+        if (resize) {
+            SDL_RenderTexture(renderer, tex_resized, 0, 0);  
+        } else {
+            SDL_RenderTexture(renderer, tex_full, 0, 0);  
+        }
 
-        SDL_RenderTexture(renderer, tex, 0, 0);
         ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
         SDL_RenderPresent(renderer);
-
+        
         // Cap to 60 fps. 
         auto now = SDL_GetTicks();
-
+        
         // For some reason, the time elapsed is almost constant. 
         // I don't know if it's my fault, or OpenCV internally caps FPS. 
         // In any case, the time_elapsed looks like it's capped at 30 FPS.
@@ -184,12 +250,12 @@ int main()
         if (time_elapsed <= 17) {
             SDL_Delay(17-time_elapsed);
         }
-
+        
         prev = now;
     }
-
+    
     println("Lower threshold: %d", lower_threshold);
     println("Upper threshold: %d", upper_threshold);
-
+    
     return 0;
 }
